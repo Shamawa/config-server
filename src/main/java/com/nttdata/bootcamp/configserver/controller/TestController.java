aee523a9ed3f832eb0bc981d21a0545cdff935c1
Alexandra Rodriguez <arodriguez@example.comI>
2022-12-19 19:56:57 +0100

    Exteriorizar y centralizar la configuración de los microservicios

---
+++ src/main/java/com/nttdata/bootcamp/configserver/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.nttdata.bootcamp.configserver.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@Value("${product.product-name}")
+	private String myProduct;
+
+	@GetMapping(path = "/getProductName")
+	public String myProduct() {
+		return this.myProduct;
+	}
+}
